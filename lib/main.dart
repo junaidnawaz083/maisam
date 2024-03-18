9e6487c58aed97fc2a6f20e09c6c3388609d4b76
Mac <mac@192.168.1.6>
2024-03-19 00:04:44 +0500

    world time zone api

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:maisam/screen/timezones.dart';
+
+void main() async {
+  runApp(GetMaterialApp(
+    home: TimeZones(),
+  ));
+}
